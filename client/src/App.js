bd5ff3f37aa3c046270720def90c39b1fc29364f
Justin E Ahlers <jusahlcoder@gmail.com>
2022-12-20 11:50:30 -0800

    Cleaning house

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Landing } from "./views/Landing";
+// import { Landing } from "./views/Landing";
 import TaskBoard from "./views/TaskBoard";
 
 export const App = () => {
